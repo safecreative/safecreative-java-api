099de2d4a236ff941674c7a079662417576908be
devprojects@safecreative.org <devprojects@safecreative.org>
2010-04-28 13:33:24 +0000

    Added basic file profile based registration example

--- safecreative-java-api-examples/run.sh
+++ safecreative-java-api-examples/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+#API Keys:
+sharedKey=7gi3unfhknsdy0gjaogsa7qbj
+privateKey=1drq9xl7y08p852pc9zxdw5ob
+
+version=0.5.0
+apiLib=../safecreative-java-api/target/safecreative-java-api-$version-SNAPSHOT-jar-with-dependencies.jar
+examplesLib=target/safecreative-java-api-examples-$version.jar
+
+class=$1
+shift 1
+java -classpath $apiLib:$examplesLib org.safecreative.api.examples.$class $sharedKey $privateKey $@
+
+
+
 
 
