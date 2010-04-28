c3a998ccd24611905459176cf715dda4eddd650d
devprojects@safecreative.org <devprojects@safecreative.org>
2010-04-28 15:55:18 +0000

    Simplified maven build

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cwd=$(pwd)
+#Build all modules
+mvn clean
+mvn install -DskipTests=true 
+#Build assembly java api
 cd safecreative-java-api
 mvn assembly:assembly -DskipTests=true 
-cd $cwd/safecreative-java-api-examples
-mvn install
-
-
+cd $cwd
