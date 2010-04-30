b1b1c6f37c191cb848c5caf78330bfd9bf223a0c
devprojects@safecreative.org <devprojects@safecreative.org>
2010-04-30 08:55:48 +0000

    Added logging properties

--- safecreative-java-api-examples/run.sh
+++ safecreative-java-api-examples/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #API Keys:
-sharedKey=<YOUR SHARED KEY>
-privateKey=<YOUR PRIVATE KEY>
+sharedKey=
+privateKey=
 
 version=0.5.0
 apiLib=../safecreative-java-api/target/safecreative-java-api-$version-SNAPSHOT-jar-with-dependencies.jar
@@ -9,7 +9,7 @@ examplesLib=target/safecreative-java-api-examples-$version.jar
 
 class=$1
 shift 1
-java -classpath $apiLib:$examplesLib org.safecreative.api.examples.$class $sharedKey $privateKey $@
+java -Dsc.api.debug=DEBUG -Dsc.api.root.debug=ERROR -classpath $apiLib:$examplesLib org.safecreative.api.examples.$class $sharedKey $privateKey $@
 
 
 
