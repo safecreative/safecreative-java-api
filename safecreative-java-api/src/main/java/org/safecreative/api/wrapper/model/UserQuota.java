89b3c1f0a33c7e6cec70f418e113de5fb1d8ec7c
vcalderon <vcalderon@safecreative.org>
2011-05-30 13:23:19 +0200

    user.quota model

---
+++ safecreative-java-api/src/main/java/org/safecreative/api/wrapper/model/UserQuota.java
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2011 Safe Creative (http://www.safecreative.org)
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.safecreative.api.wrapper.model;
+
+/**
+ * Represents user's disk and registration quota
+ * @author vcalderon@safecreative.org
+ */
+public class UserQuota {
+    private String userCode;
+    private long diskUsage;
+    private long diskQuota;
+    private long monthlyResgistrations;
+    private long monthlyResgistrationsQuota;
+
+    public long getDiskQuota() {
+        return diskQuota;
+    }
+
+    public void setDiskQuota(long diskQuota) {
+        this.diskQuota = diskQuota;
+    }
+
+    public long getDiskUsage() {
+        return diskUsage;
+    }
+
+    public void setDiskUsage(long diskUsage) {
+        this.diskUsage = diskUsage;
+    }
+
+    public long getMonthlyResgistrations() {
+        return monthlyResgistrations;
+    }
+
+    public void setMonthlyResgistrations(long monthlyResgistrations) {
+        this.monthlyResgistrations = monthlyResgistrations;
+    }
+
+    public long getMonthlyResgistrationsQuota() {
+        return monthlyResgistrationsQuota;
+    }
+
+    public void setMonthlyResgistrationsQuota(long monthlyResgistrationsQuota) {
+        this.monthlyResgistrationsQuota = monthlyResgistrationsQuota;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+}
